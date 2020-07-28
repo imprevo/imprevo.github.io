cffd3f43b979f62e4b1a58c36ead5bc18d7adca6
imprevo <zhenek42@gmail.com>
2020-07-28 22:54:42 +0700

    change name and color in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'imprevo',
+        short_name: 'imprevo',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#1b1f22',
+        theme_color: '#1b1f22',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
