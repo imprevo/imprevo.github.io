68724ca015099a0d99ebe3b009e134b6f423ce2d
imprevo <zhenek42@gmail.com>
2020-10-15 01:08:53 +0700

    removed react and gatsby

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const pluginSass = require('eleventy-plugin-sass');
 
+// TODO: manifestand favicons
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ 'src/assets/images': 'assets/images' });
   eleventyConfig.addPassthroughCopy({ 'src/assets/scripts': 'assets/scripts' });
